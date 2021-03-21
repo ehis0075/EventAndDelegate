c16212dea9b425c6149c00da03c11c69c8672358
Ehis <ehisjude420@gmail.com>
2021-03-21 15:08:36 +0100

    first commit

---
+++ src/DelegateAndEvent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DelegateAndEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1"};
+            var videoEncoder = new VideoEncoder();  //publisher
+            var mailService = new MailService();  // subscriber
+            var messageService = new MessageService(); // subscriber
+
+            // we need to do the subscription before calling the encode method
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+            //VideoEncoded behind the scenes is a list of pointers to method
+            
+
+            videoEncoder.Encode(video);
+        }
+    }
+
+    
+    
+}
